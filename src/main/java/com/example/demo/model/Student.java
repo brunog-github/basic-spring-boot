c9359dfd651899a0dafe84f7d2aacbd61636afcd
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-11-19 13:32:14 -0300

    created Student model

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,88 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.UUID;
+
+@Entity
+@Table
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Transient
+    private Integer age;
+
+    public Student() { }
+
+    public Student(String name, String email, LocalDate dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Student(UUID id, String name, String email, LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
