f6736ba5af07a2041834187746deb373cdfaede5
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-11-19 13:32:53 -0300

    created Student repository

---
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+    Optional<Student> findStudentByEmail(String email);
+
+}
